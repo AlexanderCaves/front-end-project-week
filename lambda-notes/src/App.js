89ec8f00b94ed0ba37b0a0591aec1aaff81a79ac
AlexanderCaves <ahcaves@gmail.com>
2018-09-04 10:14:05 -0700

    starting API refactor

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 
-import EditView from './components/EditView';
-import DeleteModal from './components/DeleteModal';
-import CreateNewView from './components/CreateNewView';
+
 import NoteView from './components/NoteView';
 import ListView from './components/ListView';
 import SideBar from './components/SideBar';
@@ -11,87 +10,23 @@ import './App.css';
 import './components/SideBar.css';
 import './components/NoteView.css';
 import './components/ListView.css';
-import './components/DeleteModal.css';
-import './components/CreateEditView.css';
+
+
+const url = 'https://killer-notes.herokuapp.com'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      notes: [
-        {
-          title: 'bank',
-          textBody: 'I need cash and quarters for laundry.', 
-          _id: 0,
-        },
-        {
-          title: 'laundry',
-          textBody: 'Do it late at night when no one else is trying to use the machines.',
-          _id: 1,
-        },
-        {
-          title: 'haircut',
-          textBody: 'The Supercuts on Sunset & Gower.  Call first.',
-          _id: 2,
-        },
-        {
-          title: 'cable bill',
-          textBody: 'Pay online by Thursday.',
-          _id: 3,
-        },
-        {
-          title: 'dinner',
-          textBody: "Dinner at my sister's house in Redondo Beach at 7pm.",
-          _id: 4,
-        },
-      ],
-      title: '',
-      textBody: '',
+      notes: []
     };
   }
 
-  handleInputChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  addNewNote = event => {
-    event.preventDefault();
-    const notes = [
-      ...this.state.notes,
-      {
-        _id: Date.now(),
-        title: this.state.title,
-        textBody: this.state.textBody,
-      }
-    ];
-    this.setState({
-      notes,
-      title: '',
-      textBody: '',
-    });
-  }
-
-  editNote = (event, _id) => {
-    event.preventDefault();
-
-    const notes = this.state.notes.map(eachNote => {
-      if(eachNote._id == _id) {
-        if(this.state.title.length)eachNote.title = this.state.title;
-        if(this.state.textBody.length)eachNote.textBody = this.state.textBody;
-      }
-      return eachNote;
-    });
-    this.setState({
-      notes,
-      title: '',
-      textBody: '',
-    })
-  }
-
-  deleteNote = (event, _id) => {
-    event.preventDefault();
-    const notes = this.state.notes.filter(eachNote => eachNote._id !== Number(_id));
-    this.setState({ notes });
+  componentDidMount() {
+    axios.get(url)
+      .then(response => {
+        console.log(response);
+      })
   }
 
   render() {
@@ -113,42 +48,6 @@ class App extends Component {
             <NoteView
               {...props}
               notes={this.state.notes}
-            />
-          )}
-        />
-        <Route
-          path='/create'
-          render={props => (
-            <CreateNewView
-              {...props}
-              notes={this.state.notes}
-              handleInputChange={this.handleInputChange}
-              addNewNote={this.addNewNote}
-              textBody={this.state.textBody}
-              title={this.state.title}
-            />
-          )}
-        />
-        <Route
-          path='/delete/:_id/'
-          render={props => (
-            <DeleteModal
-              {...props}
-              notes={this.state.notes}
-              deleteNote={this.deleteNote}
-            />
-          )}
-        />
-        <Route
-          path='/edit/:_id'
-          render={props => (
-            <EditView
-              {...props}
-              notes={this.state.notes}
-              handleInputChange={this.handleInputChange}
-              editNote={this.editNote}
-              textBody={this.state.textBody}
-              title={this.state.title}
             />
           )}
         />
